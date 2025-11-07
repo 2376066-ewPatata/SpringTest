573032b6969b454f286bf179ec02b83cf79a55e1
YunSeo <sophie062627777@gmail.com>
2025-11-07 18:38:02 +0900

    SpringTest<1>

---
+++ src/main/java/com/irang/springtest/hello/HelloController.java
@@ -0,0 +1,17 @@
+package com.irang.springtest.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloController {
+    @ResponseBody
+    @RequestMapping("/hello")
+        public String helloWorld(){
+            return "HelloWorld!";
+        }
+
+
+}
